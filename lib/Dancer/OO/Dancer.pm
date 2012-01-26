aed00f6236b8fa59db86ab20a0dbf54aa62d520b
Roman Galeev <ip@ncom-ufa.ru>
2012-01-26 22:28:18 +0600

    CPAN test coverage

--- lib/Dancer/OO/Dancer.pm
+++ lib/Dancer/OO/Dancer.pm
@@ -3,6 +3,8 @@ use strict;
 use Dancer ();
 use YAML;
 
+our $VERSION = '0.01';
+
 our @route_handler = qw( get post any patch del options put );
 
 sub debug {
@@ -53,4 +55,82 @@ sub import {
 	}
 }
 
-1;
+=head1 NAME
+
+Dancer::OO::Dancer - Allows to set params variables in scope of route handler
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+=head1 SYNOPSIS
+
+    use Dancer::OO::Dancer;
+
+=head1 EXPORT
+
+All Dancer methods
+
+=head1 SUBROUTINES/METHODS
+
+=head2 debug
+
+A replacement for default Dancer debug with automatic objects dumping with YAML::Dump method.
+
+=cut
+
+=head1 AUTHOR
+
+Roman Galeev, C<< <jamhedd at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-dancer-plugin-scoped at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Dancer-OO-Dancer>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Dancer::OO::Dancer
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Dancer-OO-Dancer>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Dancer-OO-Dancer>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Dancer-OO-Dancer>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Dancer-OO-Dancer/>
+
+=back
+
+=head1 ACKNOWLEDGEMENTS
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2012 Roman Galeev.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
+
+1; # End of Dancer::OO::Dancer
